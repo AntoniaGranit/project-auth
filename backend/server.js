ec9cf798ce93421564424ba1a7aa4739977527e5
AntoniaGranit <antonia.granit@gmail.com>
2023-05-23 12:11:57 +0300

    added login endpoint

--- backend/server.js
+++ backend/server.js
@@ -11,17 +11,22 @@ mongoose.Promise = Promise;
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
+    required: true,
     unique: true
   },
   email: {
     type: String,
+    format: "email",
+    minLength: 6,
+    required: true,
     unique: true
   },
   password: {
     type: String,
+    minLength: 8,
     required: true
   },
   accessToken: {
@@ -32,15 +37,7 @@ const userSchema = new Schema({
 
 const User = mongoose.model('User', userSchema);
 
-const authenticateUser = async (req, res, next) => {
-  const user = await User.findOne({accessToken:req.header('Authorization')});
-  if(user) {
-    req.user = user;
-    next();
-  } else {
-    res.status(401).json({loggedOut: true});
-  }
-}
+
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
@@ -57,6 +54,7 @@ app.get("/", (req, res) => {
   res.send(listEndpoints(app));
 });
 
+// register user:
 app.post("/register", async (req, res) => {
   const salt = bcrypt.genSaltSync(10);
   try {
@@ -73,9 +71,8 @@ app.post("/register", async (req, res) => {
       },
       message: "User successfully created!"
     })
-  } catch (err) {
-    console.log("Error:", err); {
-      // Other error
+  } catch (e) {
+    console.log("Error:", e); {
       res.status(400).json({
         success: false,
         response: err.errors,
@@ -84,6 +81,46 @@ app.post("/register", async (req, res) => {
     }
   }
 });
+
+// login:
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username: username});
+    // if the password that someone sends during login is the same as the password that is created when registering
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken
+        }
+      })
+    } else {
+      res.status(400).json({
+        success: false,
+        response: "Login failed! Please try again"
+      })
+    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      response: e
+    })
+  }
+});
+
+// authentication:
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({accessToken:req.header('Authorization')});
+  if(user) {
+    req.user = user;
+    next();
+  } else {
+    res.status(401).json({loggedOut: true});
+  }
+}
 
 app.get('/secrets', authenticateUser);
 app.get('/secrets', (req, res) => {
